ebe7ad9e0ab430ea118f13e92fc0913258c2dd77
mohammad <mohammad@localhost.localdomain>
2016-11-14 22:00:46 +0330

    custome admin started

--- ReserveForm/urls.py
+++ ReserveForm/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 
+from . import views
+
 
 urlpatterns = [
+    url(r'^adminsite/reserveForm/changeForm/(?P<id>[0-9]+)', view=views.reserveForm_change_form, name='ReserveFormChangeForm'),
 
 ]
--- ReserveForm/views.py
+++ ReserveForm/views.py
@@ -1,3 +1,25 @@
+from .models import ReserveForm, Dookht, Parche, Process, ProcessFormKargar, ServiceTarh
+from UserManager.models import Customer
+
 from django.shortcuts import render
 
-# Create your views here.
+
+
+def reserveForm_change_form(request, id):
+    form = ReserveForm.objects.get(id=id)
+    customers = Customer.objects.all()
+    services = ServiceTarh.objects.all()
+    formServices = form.serviceTarh.all()
+    context = {'currentCustomer':form.customer, 'customers':customers, 'services':services, 'formServices':formServices}
+    return render(request, 'siteAdmin/ReserveForm/change_form.html', context)
+
+
+
+
+
+
+
+
+
+
+
